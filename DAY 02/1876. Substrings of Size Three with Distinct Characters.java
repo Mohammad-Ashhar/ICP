b5188376670e646048f7f4578e4cd222715bd2ef
Mohammad_Ashhar <mohammadashhar65@gla.ac.in>
2025-09-27 19:32:36 +0530

    DAY 02 added

---
+++ DAY 02/1876. Substrings of Size Three with Distinct Characters.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countGoodSubstrings(String s) {
+        if(s.length()<3) return 0;
+        int c=0;
+        for(int i=0;i<s.length()-2;i++)
+        {
+            if(s.charAt(i)!=s.charAt(i+1) && 
+                s.charAt(i)!=s.charAt(i+2) && 
+                s.charAt(i+1)!=s.charAt(i+2))
+            {
+                c++;
+            }
+        }
+        return c;
+    }
+}
