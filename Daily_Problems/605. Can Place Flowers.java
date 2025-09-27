ab094934f01845c3a7ba0cca0f1f5019863b2d80
Mohammad Ashhar <142567321+Mohammad-Ashhar@users.noreply.github.com>
2025-09-27 07:08:20 +0530

    Add files via upload

---
+++ Daily_Problems/605. Can Place Flowers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int l=flowerbed.length;
+        for(int i=0;i<l && n>0;i++)
+        {
+            if(flowerbed[i]==0)
+            {
+                if((i==0 || flowerbed[i-1]==0) &&(i==l-1 || flowerbed[i+1]==0))
+                {
+                    flowerbed[i]=1;
+                    n--;
+                }
+            }
+        }
+        return n==0;
+        
+    }
+}
